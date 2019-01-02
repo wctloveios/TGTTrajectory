9502f5f107a5fe732a95362efa63a8034d416b1c
wct <wctloveios@163.com>
2019-01-02 20:40:31 +0800

    init project

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '8.0'
+
+target ‘TGTApp’ do
+
+    #布局库
+    pod 'Masonry', '1.1.0'
+    
+end  
+  
---
+++ TGTApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TGTApp
+//
+//  Created by mac on 2019/1/2.
+//  Copyright © 2019年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
